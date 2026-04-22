03cb6ea9e6311c022f53fb3208d636b1ceeb4e15
NoelGalgo <l22210354@tectijuana.edu.mx>
2026-04-21 20:37:00 -0700

    Configuración de Django, modelos de películas y migraciones aplicadas

--- mymovies/settings.py
+++ mymovies/settings.py
@@ -86,12 +86,8 @@ WSGI_APPLICATION = 'mymovies.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "django",
-        "USER": "ubuntu",
-        "PASSWORD": "",
-        "HOST": "/tmp",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
